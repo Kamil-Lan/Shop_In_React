1afda00177c8b6979e8c39bee778499b8b743090
Kamil Lan <kamkor50@gmail.com>
2020-06-29 18:12:15 +0200

    Finished lesson 5
    I finished lesson 5

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import { Route, Switch} from "react-router-dom";
+import { Route, Switch, Link} from "react-router-dom";
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 
 const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE </h1>
-  </div>
-)
+    <div>
+      <Link to='/topics'>Topics</Link>
+      <h1>HATS PAGE </h1>
+    </div>
+  );
+
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop/hats' component={HatsPage} />
       </Switch>
     </div>
   );
